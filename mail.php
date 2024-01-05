ad59db527e4c291373cc6300b6e53b8f5f29d038
rout <dayanetrunks@gmail.com>
2024-01-05 22:13:54 +0100

    Danny_version1.3

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$mail_me = "texto.sigded@gmail.com";
+
+// Récupérer les données envoyées depuis le client
+$data = json_decode(file_get_contents('php://input'), true);
+
+// Stocker les données dans la variable $_SESSION
+$info_mail = $data;
+
+$headers = "From: " . $info_mail['email'] . "\r\n";
+$headers .= "Reply-To: " . $info_mail['email'] . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+mail($mail_me, $info_mail['subject'], $info_mail['message'], $headers);
